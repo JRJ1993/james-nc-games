d640906e87de8b95ec40a89d601e2c21ddf4c369
James Jordan <jamesjordan@Jamess-MacBook-Pro.local>
2021-11-22 12:16:58 +0000

    saves cors update

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const apiRouter = require('./routes/apiRouter');
 const cors = require('cors')
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api", apiRouter);
