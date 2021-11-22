86c0827cc399966573c92f058de031dfea68ec6d
James Jordan <jamesjordan@Jamess-MacBook-Pro.local>
2021-11-22 12:45:32 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const apiRouter = require('./routes/apiRouter');
+
 const cors = require('cors');
-
-
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/api", apiRouter);
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,13 +2,13 @@ const apiRouter = require('express').Router();
 const categoryRouter = require('./category.router');
 const reviewsRouter = require('./reviews.router');
 const commentsRouter = require('./comments.router');
-const usersRouter = require('./users.router')
+// const usersRouter = require('./users.router')
 const {getApi} = require('../controllers/api.controller')
 
 apiRouter.route("/").get(getApi);
 apiRouter.use("/categories", categoryRouter);
 apiRouter.use("/reviews", reviewsRouter);
 apiRouter.use("/comments", commentsRouter);
-apiRouter.use("/users", usersRouter)
+// apiRouter.use("/users", usersRouter)
 
 module.exports = apiRouter;
